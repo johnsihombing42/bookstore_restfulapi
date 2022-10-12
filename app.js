891abff7a066db7702b74d2e13816852cc805497
johnsihombing42 <johnsihombing42@gmail.com>
2022-10-12 22:04:25 +0700

    init project

--- app.js
+++ app.js
@@ -8,3 +8,6 @@ app.use(morgan("dev"));
 
 const { HTTP_PORT } = process.env;
 app.listen(HTTP_PORT, () => console.log("Listening on port", HTTP_PORT));
+
+sequelize model:generate --name book --attributes title:string,description:string,author:string,publisher:string
+sequelize model:generate --name user --attributes username:string,password:string
