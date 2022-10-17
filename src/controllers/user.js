caab81370a2ed9bb541ce8ad4c96e5b1d54dad04
johnsihombing42 <johnsihombing42@gmail.com>
2022-10-17 22:32:47 +0700

    add register

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,10 +1,19 @@
 const { user } = require("../db/models");
-
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 module.exports = {
-  get: (req, res, next) => {
-    return res.status(200).json({
-      status: true,
-      message: "Halo",
-    });
+  registerUser: async (req, res, next) => {
+    try {
+      const { username, password } = req.body;
+      const exixtUser = await user.findOne({
+        where: { username: username },
+      });
+      if (existUser) {
+        return res.status(409).json({
+          status: false,
+          message: "Username already used",
+        });
+      }
+    } catch {}
   },
 };
