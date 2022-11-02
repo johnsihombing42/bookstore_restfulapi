528f2b1b1b07e233ea1c59b3b81d3c63703ba997
johnsihombing42 <johnsihombing42@gmail.com>
2022-11-02 22:28:12 +0700

    update route

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,5 +4,7 @@ const userController = require("../controllers");
 const middleware = require("../helper/middleware");
 
 router.get("/", userController.user.read);
+router.put("/", userController.user.update);
+router.delete("/", userController.user.delete);
 
 module.exports = router;
